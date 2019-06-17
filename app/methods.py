797f5e9ee65e5f9da62378c3409def20d912e6c3
Zach Urich <re.jo@live.com>
2019-06-16 20:04:33 -0500

    store scrape results in s3 for caching

--- app/methods.py
+++ app/methods.py
@@ -1,7 +1,7 @@
 import falcon
 import json
 import app.common as common
-from app.resources import (send_email, fetch_html)
+from app.resources import (send_email, fetch_html, update_cache, read_cache)
 from app.scraper import (scrape_product_data, format_results)
 import smtplib
 
@@ -28,8 +28,9 @@ def execute():
     try:
         html = fetch_html(common.SCRAPE_SOURCE)
         results = scrape_product_data(html, True)
-        print(len(results))
-        if len(results) > 0:
+        cache = read_cache()
+        if results != cache and len(results) > 0:
+            update_cache(results)
             send_email(format_results(results))
         return results
     except Exception as e:
--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,9 @@
 import os
 import json
 import requests
+import boto3
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 def fetch_html(source):
@@ -14,7 +17,7 @@ def fetch_html(source):
 
 
 def send_email(content):
-    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + "/auth.json") as auth:
+    with open(root_dir + "/auth.json") as auth:
         data = json.load(auth)
         MAILGUN_DOMAIN = data.get('mailgun-domain')
         MAILGUN_API_KEY = data.get('mailgun-api-key')
@@ -30,3 +33,28 @@ def send_email(content):
         )
     except Exception as e:
         raise e
+
+
+def update_cache(data):
+    s3_bucket = get_s3_bucket_name()
+    tmp_file_path = root_dir + "/cache.json"
+    output_file = "cache.json"
+    s3 = boto3.client('s3')
+    with open(tmp_file_path, 'w+') as file:
+        file.write(json.dumps(data))
+    s3.upload_file(tmp_file_path, s3_bucket, output_file)
+
+
+def read_cache():
+    s3_bucket = get_s3_bucket_name()
+    output_file = "cache.json"
+    s3 = boto3.resource('s3')
+    file_data = s3.Object(s3_bucket, output_file)
+    content = file_data.get()['Body'].read().decode('utf-8')
+    return json.loads(content)
+
+
+def get_s3_bucket_name():
+    with open(root_dir + "/auth.json") as auth:
+        s3_bucket = json.load(auth).get('s3-bucket-name')
+    return s3_bucket
